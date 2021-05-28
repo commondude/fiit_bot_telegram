0555b1edc584421195a47b85223eac15646aee5f
Ilia Yartsev <ilia.yartsev@gmail.com>
2021-05-28 19:53:52 +0300

    ORM invasion clean + train verbs

--- flaskbotPostgre.py
+++ flaskbotPostgre.py
@@ -1,43 +1,50 @@
 import datetime # библиотка для работы с датой и временем
 import pytz # библиотека для работы с часовыми поясами
-import json # библиотка для работы со стандартом json
+# import json # библиотка для работы со стандартом json
 from threading import Thread # Модуль для работы с потоками
+# from pickle import dumps,loads
+from json import dumps,loads
 
 import jokeAndWeather # Погода и анекдоты
 from time import sleep # функция для создания паузы в выполнении программы
 import talk2telegramapi # модуль с функциями общения с API Telegram
 import fiitinfo  # Модуль с информацией по группе Фиит
 import misc # Модуль с токеном бота
+import train_verbs # Модуль с тренировкой англ глаголов
 
 
-from os import environ # модуль для работы с переменными окружения
+from os import environ # модуль для работы с переменными окружения для heroku
 import psycopg2 # Модуль для работы с БД PostgreSQL
 
 # Модуль с вебсервером Flask
 from flask import Flask
 from flask import request
 
+from sqlalchemy import *
+from sqlalchemy.orm import sessionmaker,declarative_base
 
 # Функция для записи данных в БД
 def write_2_db(fiit_dictionary_loc):
 	try:
-		cur.execute("UPDATE last_state SET updown = %s,schedule_list = %s,last_update_id = %s,init_week = %s,restart_count = %s, send_schedule_bool =%s;",(
-		fiit_dictionary_loc['updown'],fiit_dictionary_loc['schedule_chat_list'],fiit_dictionary_loc['last_upd_id'],
-		fiit_dictionary_loc['init_week'],fiit_dictionary_loc['restart_count'],fiit_dictionary_loc['send_schedule_bool']))
-		conn.commit()
-		cur.execute("SELECT * FROM last_state WHERE id = 1;")
-		print(list(cur.fetchone()))
+		#sqlalchemy
+		lst.updown = fiit_dictionary_loc['updown']
+		lst.schedule_list = dumps(fiit_dictionary_loc['schedule_chat_list'])
+		lst.last_update_id = fiit_dictionary_loc['last_upd_id']
+		lst.init_week = fiit_dictionary_loc['init_week']
+		lst.restart_count = fiit_dictionary_loc['restart_count']
+		lst.send_schedule_bool = fiit_dictionary_loc['send_schedule_bool']
+		session.commit()
 		return True
-
+		#init_week = %s, 		fiit_dictionary_loc['init_week'],
 	except Exception as e:
 		print(e)
 		return False
 
 
-
 # Функция команды schedule_init
 def do_schedule_init(chat_id,fiit_dictionary_loc):
 	if chat_id not in fiit_dictionary_loc['schedule_chat_list']:
+		print(type(fiit_dictionary_loc['schedule_chat_list']))
 		fiit_dictionary_loc['schedule_chat_list'].append(chat_id)
 		write_2_db(fiit_dictionary_loc)
 		talk2telegramapi.send_message(chat_id,'Ваш чат добавлен в список рассылки.')
@@ -81,24 +88,49 @@ def do_init_down(chat_id):
 
 DATABASE_URL = environ['DATABASE_URL']
 
+"""sqlalchemy"""
+connection_string = 'postgresql'+DATABASE_URL[8:]
 try:
-	conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+	engine = create_engine(connection_string,echo=False)
+	Session = sessionmaker(bind=engine)
 	talk2telegramapi.send_message('169294743','Коннект к БД успешный')
-
 except:
 	talk2telegramapi.send_message('169294743','Не могу законнектиться к БД')
 	print('Не могу законнектиться к БД')
 
-cur = conn.cursor()
-cur.execute("SELECT * FROM last_state;")
-select_data=list(cur.fetchone())
+session = Session()
+Base = declarative_base()
+
+"""ORM класс таблицы состояния приложения DataBase first"""
+class LastState(Base):
+	__tablename__ = 'last_state'
+
+	id = Column(Integer, primary_key=True)
+	updown = Column(Text)
+	schedule_list = Column(Text)
+	last_update_id = Column(Text)
+	init_week = Column(Boolean)
+	restart_count = Column(Integer)
+	send_schedule_bool = Column(Boolean)
+
+	def __repr__(self):
+		return "<LastState(updown='%s', schedule_list='%s', last_update_id='%s', init_week='%s', restart_count='%s', send_schedule_bool='%s')>"%(
+				self.updown,self.schedule_list,self.last_update_id,self.init_week,self.restart_count,self.send_schedule_bool)
+
+lst=session.query(LastState).filter(LastState.id==1).first()
 fiit_dictionary = {}
-fiit_dictionary.update({'updown':select_data[1]})
-fiit_dictionary.update({'schedule_chat_list':select_data[2]})
-fiit_dictionary.update({'last_upd_id':select_data[3]})
-fiit_dictionary.update({'init_week':select_data[4]})
-fiit_dictionary.update({'restart_count':select_data[5]+1})
-fiit_dictionary.update({'send_schedule_bool':select_data[6]})
+fiit_dictionary.update({'updown':lst.updown})
+if (lst.schedule_list != ''):
+	list_of_chats =loads(lst.schedule_list)
+	print(list_of_chats)
+	fiit_dictionary.update({'schedule_chat_list':list_of_chats})
+else:
+	fiit_dictionary.update({'schedule_chat_list':[]})
+fiit_dictionary.update({'last_upd_id':lst.last_update_id})
+fiit_dictionary.update({'init_week':lst.init_week})
+fiit_dictionary.update({'restart_count':lst.restart_count+1})
+fiit_dictionary.update({'send_schedule_bool':lst.send_schedule_bool})
+
 if fiit_dictionary['updown']=='up':
 	schedule = fiitinfo.schedule_up
 else:
@@ -106,54 +138,11 @@ else:
 
 
 """Сразу записываем в бд изменённый счётчик рестартов"""
+write_2_db(fiit_dictionary)
 
-cur.execute("UPDATE last_state SET restart_count = %d;"%(fiit_dictionary['restart_count']))
-conn.commit()
 
-print(fiit_dictionary['restart_count'])
-
-# Задаём словарь информационных команд
-dict_of_commands = {
-    # Информация о деканате
-    '/dean':'Информация о деканате',
-    '/dean@FiitRndBot':'Информация о деканате',
-    # Информация о дисциплинах
-    '/disciplines':'Информация о дисциплинах',
-    '/disciplines@FiitRndBot':'Информация о дисциплинах',
-    # Информация о сессии
-    '/session':'Информация о сессии',
-    '/session@FiitRndBot':'Информация о сессии',
-    # Спсиок группы с контактами
-    '/group_list':"Спсиок группы с контактами",
-    '/group_list@FiitRndBot': "Спиcок группы с контактами",
-    # Инизиализация  рассылки расписания
-    '/schedule_init':'Инизиализация рассылки расписания',
-    '/schedule_init@FiitRndBot':'Инизиализация рассылки расписания',
-    # Остановка рассылки расписания
-    '/schedule_stop':'Остановка рассылки расписания',
-    '/schedule_stop@FiitRndBot':'Остановка рассылки расписания',
-    # Инициализация верхней недели
-    '/init_up':'Инициализация верхней недели',
-    '/init_up@FiitRndBot':'Инициализация верхней недели',
-    # Инициализация нижней недели
-    '/init_down':'Инициализация нижней недели',
-    '/init_down@FiitRndBot':'Инициализация нижней недели',
-    # Расписание на неделю
-    '/schedule_week':'Расписание на эту неделю',
-    '/schedule_week@FiitRndBot':'Расписание на эту неделю',
-    # Службная команда
-    '/vars':'Значения переменных в стандартный вывод',
-    '/vars@FiitRndBot':'Значения переменных в стандартный вывод',
-    # Погода в Ростове
-    '/weather':'Прогноз погоды',
-    '/weather@FiitRndBot':'Прогноз погоды',
-    # Анекдот
-    '/joke':'Анекдот',
-    '/joke@FiitRndBot':'Анекдот'
-}
-
+#Получаем время для рассылки расписания Время Московское
 servertz = pytz.timezone("Europe/Moscow")
-
 # Определяем функцию отдельного потока
 def delivery(text):
 	global fiit_dictionary
@@ -162,35 +151,40 @@ def delivery(text):
 	while True:
 			# Переманная текущего дня и времени по Москве
 			now = datetime.datetime.now(servertz)
-			if (now.weekday() in range(5)) and (now.hour == 17) and (not fiit_dictionary['send_schedule_bool']) and (now.minute == 30):
+			if (now.weekday() in range(6)) and (now.hour == 13) and (not fiit_dictionary['send_schedule_bool']) and (now.minute == 30):
 				for i in fiit_dictionary['schedule_chat_list']:
-					talk2telegramapi.send_message(i, 'Можно отписаться до осени!:) А завтра в 14.30 выпить пивка!')
-					# talk2telegramapi.send_message(i, 'Неделя = '+fiit_dictionary['updown'])
-					# talk2telegramapi.send_message(i, ','.join(schedule[now.weekday()]))
+					#talk2telegramapi.send_message(i, 'Можно отписаться до осени!:) А завтра в 14.30 выпить пивка!')
+					talk2telegramapi.send_message(i, 'Неделя = '+fiit_dictionary['updown'])
+					talk2telegramapi.send_message(i, ','.join(schedule[now.weekday()]))
 				fiit_dictionary['send_schedule_bool'] = True
 				write_2_db(fiit_dictionary)
-
-			# В конце недели меням значения флага инизиализации недели
-			if now.weekday() == 6 and now.hour ==23 and now.minute ==59 and now.second >= 55 :
-				fiit_dictionary['init_week'] = False
-				#sleep(10)#Избегаем повторноо переключения недели. Изменение этой перевнной должно быть 1 раз в неделю
+			# Смена типа недели.
+			if now.weekday() == 6 and now.hour ==23 and now.minute ==59 and now.second >= 55:
+				if (fiit_dictionary['updown']=='up'):
+					fiit_dictionary['updown']='down'
+				else:
+					fiit_dictionary['updown']='up'
 				write_2_db(fiit_dictionary)
+				sleep(10)#Избегаем повторноо переключения недели. Изменение этой перевнной должно быть 1 раз в неделю
 
 			# В 23 часу сбрасываем флаг отправки расписания
 			if now.weekday() in range(5) and now.hour == 16 and fiit_dictionary['send_schedule_bool']:
 				fiit_dictionary['send_schedule_bool'] = False
 				write_2_db(fiit_dictionary)
+
 # Создаём и стартуем отдельный поток
 thread1 = Thread(target=delivery,args=('',))
 thread1.deamon=True
 thread1.start()
 
-# Создаём переменную с экземпляром flask
 
+
+
+
+# Создаём переменную с экземпляром flask
 app = Flask(__name__)
 
 #Начало маршрутов веб сервера
-
 @app.route('/')
 def hello_world():
 	return 'Hello World!'
@@ -208,18 +202,17 @@ def update_in():
 			message ={'update_id':1, 'chat_id':'169294743','text':'ОщиПка'}
 		#talk2telegramapi.send_message(r['message']['chat']['id'],json.dumps(message, ensure_ascii=False))
 
-
-
 		if message:
 			fiit_dictionary['last_upd_id'] = message['update_id']
 			write_2_db(fiit_dictionary)
 
 			# Присваиваем переменным значения из вновь пришедшего сообщения
 			msg_text = message['text']
-			msg_chat_id = message['chat_id']
+			msg_chat_id = str(message['chat_id'])
+			print(type(msg_chat_id))
 
 			# Переписываем выбор команды
-			if msg_text in dict_of_commands:
+			if msg_text in fiitinfo.dict_of_commands:
 				# Инизиализация и остановка рассылки расписания
 				if (msg_text == '/schedule_init' or msg_text == '/schedule_init@FiitRndBot'):
 					fiit_dictionary=do_schedule_init(msg_chat_id,fiit_dictionary)
@@ -255,12 +248,12 @@ def update_in():
 				elif (msg_text == '/group_list' or msg_text == '/group_list@FiitRndBot'):
 					grouplist = ''
 					for i in fiitinfo.students:
-						grouplist += i['firstname']+' '+i['surname']+' '+i['patronymic']+' '+'\n'+i['phone']+'\n'+'Номер в журнале'+i['journal_number']+'\n'+'\n'
+						grouplist += i['firstname']+' '+i['surname']+' '+i['patronymic']+' '+'\n'+i['phone']+'\n'+'Номер в журнале : '+i['journal_number']+'\n'+'\n'
 					talk2telegramapi.send_message(msg_chat_id,grouplist)
 
 				elif (msg_text == '/schedule_week' or msg_text == '/schedule_week@FiitRndBot'):
 					schedule_week = ''
-					for i in range(0,5):
+					for i in range(0,6):
 						schedule_week += fiitinfo.week_days[i]+'\n'+ str(schedule[i]) +'\n'+'\n'
 					talk2telegramapi.send_message(msg_chat_id,schedule_week)
 
@@ -282,6 +275,10 @@ def update_in():
 				elif (msg_text == '/joke' or msg_text == '/joke@FiitRndBot'):
 					talk2telegramapi.send_message(msg_chat_id,jokeAndWeather.joke())
 
+				# Тренируем английский
+				elif (msg_text == '/train_verbs' or msg_text == '/train_verbs@FiitRndBot'):
+					talk2telegramapi.send_message(msg_chat_id,train_verbs.rand_sentence()[0])
+
 				else:
 					talk2telegramapi.send_message(msg_chat_id,'Кто-то забыл прописать функцию для команды.')
 
@@ -301,7 +298,7 @@ def update_in():
 if __name__ == '__main__':
 	port = int(environ.get('PORT', 5000))
 	app.run(host='0.0.0.0', port=port)
-	# app.run()
 
-cur.close()
-conn.close()
+
+#Закрываем сессию работы с бд
+session.close()
